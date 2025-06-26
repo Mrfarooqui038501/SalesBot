c146e6cc652694d2a6d04c0311107514074dd24f
Arman <armanfaruque786@gmail.com>
2025-06-26 13:32:37 +0530

    pushing on git for deployment

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,46 @@
+import axios from 'axios';
+
+
+const api = axios.create({
+  baseURL: 'https://saleschatbot-s7sz.onrender.com/api', 
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 10000, 
+});
+
+
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    console.log('Making request to:', config.baseURL + config.url); 
+    return config;
+  },
+  (error) => {
+    console.error('Request interceptor error:', error);
+    return Promise.reject(error);
+  }
+);
+
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    console.error('API Error:', error.response?.data || error.message);
+    
+    
+    if (error.response?.status === 401) {
+      localStorage.removeItem('token');
+     
+    }
+    
+    return Promise.reject(error);
+  }
+);
+
+export default api;
